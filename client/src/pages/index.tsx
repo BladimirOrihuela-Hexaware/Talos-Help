75a5b7c22629c16adc24523e334c04b874c38d18
Bladimir Orihuela <bladimiro1@hexaware.com>
2023-04-20 14:02:12 -0600

    update index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import styled from "styled-components";
-
-const BlueSquare = styled.div`
-  background-color: blue;
-  border: 1px solid red;
-  height: 20vh;
-  width: 30vw;
-`;
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@common/constants/colors";
+import { Text } from "@common/components/Text";
 
 const Index = () => {
-  return <BlueSquare />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Text type="h3">Index.tsx</Text>
+        </ThemeProvider>
+    );
 };
 
 export default Index;
