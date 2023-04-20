0dacd3a345b2fa52be3d841bd43abba8bed47372
Bladimir Orihuela <bladimiro1@hexaware.com>
2023-04-20 14:03:00 -0600

    💄 Add Text component

---
+++ client/src/common/components/Text.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { PropsWithChildren } from "react";
+import Box from "@mui/material/Box";
+import Typography, { TypographyProps } from "@mui/material/Typography";
+
+const variants = {
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    sub: "subtitle1",
+    body: "body2",
+} as const;
+
+type TextType = keyof typeof variants;
+type Variant = typeof variants[TextType];
+
+const getTextVariant = (type?: TextType): Variant => {
+    if (!type) return variants.body;
+    return variants[type];
+};
+
+type TextProps = {
+    type?: TextType;
+} & TypographyProps;
+
+export const Text = (props: TextProps) => {
+    return (
+        <Typography {...props} variant={getTextVariant(props.type)}>
+            {props.children}
+        </Typography>
+    );
+};
