4ef282d42e536810cfcb44b14e2a35d91b1b848c
Benjamín Guzmán <BenjaminV@hexaware.com>
2023-04-22 16:12:57 -0600

    [backend][licensing]: Added `generator` table to ddl

--- backend/licensing/db/00-ddl.sql
+++ backend/licensing/db/00-ddl.sql
@@ -4,9 +4,18 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE org(
     id      uuid DEFAULT uuid_generate_v4(),
-    name    VARCHAR NOT NULL,
+    name    VARCHAR UNIQUE NOT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE generator(
+    id              uuid DEFAULT uuid_generate_v4(),
+    active          BOOLEAN NOT NULL DEFAULT true,
+    email           VARCHAR NOT NULL UNIQUE,
+    registered_at   TIMESTAMPTZ NOT NULL DEFAULT clock_timestamp(),
+    PRIMARY KEY(id)
+);
+COMMENT ON TABLE generator IS 'people inside this table are allowed to generate licenses';
 
 CREATE TABLE license(
     id              uuid DEFAULT uuid_generate_v4(),
@@ -16,8 +25,11 @@ CREATE TABLE license(
     expiration      DATE NOT NULL,
     client_timeout  INT NOT NULL CHECK (client_timeout >= 0),
     org_id          uuid NOT NULL,
+    generated_at    DATE NOT NULL DEFAULT CURRENT_DATE,
+    generator_id    uuid NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT license_fk_org FOREIGN KEY(org_id) REFERENCES org(id)
+    CONSTRAINT license_fk_org FOREIGN KEY(org_id) REFERENCES org(id),
+    CONSTRAINT license_fk_generator FOREIGN KEY(generator_id) REFERENCES generator(id)
 );
 COMMENT ON COLUMN license.max_clients IS 'max_clients >= -1 at all times. -1 means "unlimited clients"';
 COMMENT ON COLUMN license.client_timeout IS 'client_timeout >= 0 at all times. 0 means "no timeout"';
