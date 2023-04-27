f6d92c5bf7c644ebe9b2a9abfd783f8b42388491
Eduardo Castillo Hexaware <EduardoC@hexaware.com>
2023-04-27 17:14:14 -0600

    Created unit tests for actions service

--- backend/actions/src/actions.controller.spec.ts
+++ backend/actions/src/actions.controller.spec.ts
@@ -37,10 +37,45 @@ describe("Action Controller", () => {
     });
 
     it("should return a found action", () => {
-        const { Action } = icontroller.getAction("accept");
-        expect(Action).toMatchObject({ actionName: "Accept", description: "Clicks on the 'OK' button as soon as any pop up window / alert appears." });
-    });
-
+        const { Action } = icontroller.getAction("alertaccept");
+        expect( Action ).toMatchObject({ 
+            actionName:"Alert Accept",
+            description: 
+                "Clicks on the 'OK' button as soon as any pop up window / alert appears.",
+            isWeb: true,
+            isMobile: true,
+            isDesktop: false,
+            message:{
+                messageType:"information",
+                description:
+                    "An alert/pop up is a small message box which appears on screen to give the user some information or notification."
+            },
+            parameters:[
+                {
+                    title:"Value",
+                    description:"Not Required"
+                },{		
+                    title:"Locator Type",
+                    description:"Not Applicable",
+                },{	
+                    title:"Locator Value",
+                    description:"Not Applicable",
+                },{	
+                    title:"Known Issue",
+                    description:"Not Applicable"		
+                }],
+                example:{
+                    description:
+                    "In the following image shows the example of how to use the Alert Accept Action in the step #5 of the Test Case example.",
+                    img:""
+                },
+                messageWithTCFile:{
+                    messageType:"successful",
+                    description:"For more information regarding this action, please download the following Talos TC file and import it to your TALOS Client.",
+                    TCfile:"Path for the Test Case.stmtc"
+                }
+            });
+        });
     it("should return not found error", () => {
         try {
             icontroller.getAction("invalid id");
--- backend/actions/src/responses/actions.res.ts
+++ backend/actions/src/responses/actions.res.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { ActionBase } from "../models/actions_base.entity";
-import {Action} from "src/models/action.entity";
+import {Action} from "../models/action.entity";
 
 export class ActionsResponse {
     @ApiProperty({ description: "List of Actions" })
