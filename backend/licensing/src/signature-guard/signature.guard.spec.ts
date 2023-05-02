0fee7796987edd4117e87e4031a8ae5ff62b82a7
BenjaminGuzman-Hexaware <108829034+BenjaminGuzman-Hexaware@users.noreply.github.com>
2023-05-02 10:40:29 -0600

    Delete signature.guard.spec.ts

--- backend/licensing/src/signature-guard/signature.guard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { SignatureGuard } from "./signature.guard";
-import { createMock } from "@golevelup/ts-jest";
-
-describe("SignatureGuard", () => {
-    it("should be defined", () => {
-        expect(new SignatureGuard()).toBeDefined();
-    });
-});
