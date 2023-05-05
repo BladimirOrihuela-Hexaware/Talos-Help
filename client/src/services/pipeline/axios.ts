f0e60757dda5414ab11449d7c9c49da50f8c0860
Bladimir Orihuela <bladimiro1@hexaware.com>
2023-05-05 10:53:20 -0600

    Update features port & set base url in client

--- client/src/services/pipeline/axios.ts
+++ client/src/services/pipeline/axios.ts
@@ -1,31 +1,58 @@
 import axios from "axios";
 import { Config } from "./types";
 
-const BasePath = "https://taloshelp.dev";
+const BasePath = process.env.NEXT_PUBLIC_BASE_PATH;
 const Version = "latest";
 
 export class Pipeline {
-    constructor() {}
+    dev: boolean;
+    constructor() {
+        this.dev = process.env.NODE_ENV === "development";
+    }
 
     async request(config: Config) {
-        const response = await axios({
-            method: config.method,
-            baseURL: BasePath,
-            url: this.buildURL(config),
-            data: config.data,
-        });
+        try {
+            const response = await axios({
+                method: config.method,
+                baseURL: this.dev ? `${BasePath}:${this.mapPort(config)}` : BasePath,
+                url: this.buildURL(config),
+                data: config.data,
+                transformResponse: [
+                    (data) => {
+                        return data;
+                    },
+                ],
+            });
 
-        //TODO: Apply validation to request
-        const { responseType } = config;
-        type ResponseType = typeof responseType;
-        console.log(response.data);
+            //TODO: Apply validation to response
+            const formated = JSON.parse(response.data);
 
-        return response.data;
+            return formated;
+        } catch (error) {
+            return undefined;
+        }
     }
 
     private buildURL(config: Config): string {
         const { service, id, route } = config;
-        if (id && route) return `${service}/${Version}/${route}/${id}`;
-        return `${service}/${Version}`;
+        let url = `${service}/${Version}/${route}`; // features/latest/integrations
+        if (id) return `${url}/${id}`; // features/latest/integration/genrocket
+        return url;
+    }
+
+    /**
+     * Dev only
+     */
+    private mapPort({ service }: Config): number {
+        switch (service) {
+            case "actions":
+                return 3000;
+            case "features":
+                return 3001;
+            case "licensing":
+                return 3000;
+            default:
+                return 3000;
+        }
     }
 }
